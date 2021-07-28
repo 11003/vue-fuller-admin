8b7a218aae312be3c700b4b393b254bde1d7b37a
liuhai <liuhai123>
2021-07-28 15:40:47 +0800

    vuex持久化

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import getters from './getters'
-
+import createPersistedState from 'vuex-persistedstate';
 Vue.use(Vuex)
 
 // https://webpack.js.org/guides/dependency-management/#requirecontext
@@ -19,7 +19,20 @@ const modules = modulesFiles.keys().reduce((modules, modulePath) => {
 
 const store = new Vuex.Store({
   modules,
-  getters
+  getters,
+  // 数据持久化
+  plugins: [
+    createPersistedState({
+      storage: window.localStorage,
+      reducer(val) {
+        return {
+          app: val.app,
+          user: val.user,
+          settings: val.settings
+        }
+      }
+    }),
+  ],
 })
 
 export default store
